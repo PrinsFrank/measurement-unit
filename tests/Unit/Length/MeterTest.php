3c0912df35cae7be8eb00fd670bbdde4fd4a3703
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2022-10-22 21:44:26 +0200

    Add UT for Meter class

---
+++ tests/Unit/Length/MeterTest.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace PrinsFrank\MeasurementUnit\Tests\Unit\Speed;
+
+use PHPUnit\Framework\TestCase;
+use PrinsFrank\ArithmeticOperations\ArithmeticOperations;
+use PrinsFrank\MeasurementUnit\Length\Meter;
+
+/**
+ * @coversDefaultClass \PrinsFrank\MeasurementUnit\Length\Meter
+ */
+class MeterTest extends TestCase
+{
+    /**
+     * @covers ::getSymbol
+     */
+    public function testGetSymbol(): void
+    {
+        static::assertSame('m', Meter::getSymbol());
+    }
+
+    /**
+     * @covers ::toMeterPerSecondValue
+     */
+    public function testToMeterPerSecondValue(): void
+    {
+        static::assertSame(42.0, Meter::toMeterValue(42.0, $this->createMock(ArithmeticOperations::class)));
+    }
+
+    /**
+     * @covers ::fromMeterPerSecondValue
+     */
+    public function testFromMeterPerSecondValue(): void
+    {
+        static::assertSame(42.0, Meter::fromMeterValue(42.0, $this->createMock(ArithmeticOperations::class)));
+    }
+}
