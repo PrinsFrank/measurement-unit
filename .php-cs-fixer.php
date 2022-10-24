84cc248c8d20a0975beecbb15fa9731cf50db8a5
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2022-10-24 20:24:32 +0200

    Setup csfixer

---
+++ .php-cs-fixer.php
@@ -0,0 +1,19 @@
+<?php
+
+return (new PhpCsFixer\Config())->setRules(
+    [
+        '@PSR12'                       => true,
+        'declare_strict_types'         => true,
+        'strict_comparison'            => true,
+        'strict_param'                 => true,
+        'blank_line_after_opening_tag' => false,
+        'no_extra_blank_lines'         => true,
+        'class_attributes_separation'  => true,
+        'array_syntax'                 => ['syntax' => 'short'],
+        'binary_operator_spaces'       => ['default' => 'align_single_space']
+    ]
+)->setFinder(
+    PhpCsFixer\Finder::create()
+        ->exclude('vendor')
+        ->in(__DIR__)
+);
