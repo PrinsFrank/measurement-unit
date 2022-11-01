70235ea575bb2de31f6e8bae106e4ab9ff1fe0dd
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2022-11-01 21:19:49 +0100

    Add missing tests for KilometerPerHour

--- tests/Unit/Speed/KilometerPerHourTest.php
+++ tests/Unit/Speed/KilometerPerHourTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace PrinsFrank\MeasurementUnit\Tests\Unit\Speed;
 
 use PHPUnit\Framework\TestCase;
+use PrinsFrank\ArithmeticOperations\ArithmeticOperations;
 use PrinsFrank\MeasurementUnit\Speed\KilometerPerHour;
 
 /**
@@ -17,5 +18,33 @@ class KilometerPerHourTest extends TestCase
     public function testGetSymbol(): void
     {
         static::assertSame('km/h', KilometerPerHour::getSymbol());
+    }
+
+    /**
+     * @covers ::fromMeterPerSecondValue
+     */
+    public function testFromMeterPerSecondValue(): void
+    {
+        $arithmeticOperations = $this->createMock(ArithmeticOperations::class);
+        $arithmeticOperations->expects(self::once())
+                             ->method('divide')
+                             ->with(42.0, 0.277778)
+                             ->willReturn(151.19987904);
+
+        static::assertSame(151.19987904, KilometerPerHour::fromMeterPerSecondValue(42.0, $arithmeticOperations));
+    }
+
+    /**
+     * @covers ::toMeterPerSecondValue
+     */
+    public function testToMeterPerSecondValue(): void
+    {
+        $arithmeticOperations = $this->createMock(ArithmeticOperations::class);
+        $arithmeticOperations->expects(self::once())
+                             ->method('multiply')
+                             ->with(42.0, 0.277778)
+                             ->willReturn(11.666676);
+
+        static::assertSame(11.666676, KilometerPerHour::toMeterPerSecondValue(42.0, $arithmeticOperations));
     }
 }
