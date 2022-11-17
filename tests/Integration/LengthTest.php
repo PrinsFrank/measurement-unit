df7d8039ae5ae00222f5e23cb9dc29b880b9e3b0
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2022-11-17 22:30:01 +0100

    Add conversion tests for lengths

--- tests/Integration/LengthTest.php
+++ tests/Integration/LengthTest.php
@@ -11,9 +11,9 @@ use PrinsFrank\MeasurementUnit\Length\HorseLength;
 use PrinsFrank\MeasurementUnit\Length\Inch;
 use PrinsFrank\MeasurementUnit\Length\Length;
 use PrinsFrank\MeasurementUnit\Length\Meter;
-use PrinsFrank\MeasurementUnit\Length\Mile;
-use PrinsFrank\MeasurementUnit\Length\NauticalMile;
 use PrinsFrank\MeasurementUnit\Length\StatuteMile;
+use PrinsFrank\MeasurementUnit\Length\NauticalMile;
+use PrinsFrank\MeasurementUnit\Length\SurveyMile;
 use PrinsFrank\MeasurementUnit\Length\Thou;
 use PrinsFrank\MeasurementUnit\Length\Yard;
 
@@ -28,9 +28,9 @@ class LengthTest extends TestCase
         HorseLength::class,
         Inch::class,
         Meter::class,
-        Mile::class,
-        NauticalMile::class,
         StatuteMile::class,
+        NauticalMile::class,
+        SurveyMile::class,
         Thou::class,
         Yard::class,
     ];
@@ -47,5 +47,20 @@ class LengthTest extends TestCase
         foreach (self::LENGTH_FQN_S as $lengthFQN) {
             yield $lengthFQN => [new $lengthFQN(42.0)];
         }
+    }
+
+    public function testCorrectConversionRate(): void
+    {
+        static::assertEqualsWithDelta(new Meter(76.8096), (new Fathom(42.0))->toMeter(), 0.000001);
+        static::assertEqualsWithDelta(new Meter(12.8016), (new Foot(42.0))->toMeter(), 0.000001);
+        static::assertEqualsWithDelta(new Meter(8449.056), (new Furlong(42.0))->toMeter(), 0.000001);
+        static::assertEqualsWithDelta(new Meter(100.8), (new HorseLength(42.0))->toMeter(), 0.000001);
+        static::assertEqualsWithDelta(new Meter(1.0668), (new Inch(42.0))->toMeter(), 0.000001);
+        static::assertEqualsWithDelta(new Meter(42.0), (new Meter(42.0))->toMeter(), 0.000001);
+        static::assertEqualsWithDelta(new Meter(67592.448), (new StatuteMile(42.0))->toMeter(), 0.000001);
+        static::assertEqualsWithDelta(new Meter(77784), (new NauticalMile(42.0))->toMeter(), 0.000001);
+        static::assertEqualsWithDelta(new Meter(67592.5824), (new SurveyMile(42.0))->toMeter(), 0.000001);
+        static::assertEqualsWithDelta(new Meter(0.0010668), (new Thou(42.0))->toMeter(), 0.000001);
+        static::assertEqualsWithDelta(new Meter(38.4048), (new Yard(42.0))->toMeter(), 0.000001);
     }
 }
