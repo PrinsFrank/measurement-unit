9e4ad6b55e77ff23d8eed3a9e33abfd9d62cc974
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2024-07-26 23:49:44 +0200

    Fix incorrect PSR4 namespaces in test cases (#6)

--- tests/Unit/Temperature/TemperatureTest.php
+++ tests/Unit/Temperature/TemperatureTest.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace PrinsFrank\MeasurementUnit\Tests\Unit\Speed;
+namespace PrinsFrank\MeasurementUnit\Tests\Unit\Temperature;
 
 use PHPUnit\Framework\TestCase;
 use PrinsFrank\ArithmeticOperations\ArithmeticOperations;
