cc3c809a8b774af9529de148debc97682f242e4a
PrinsFrank <github-action@actions-template-sync.noreply.github.com>
2024-09-18 19:58:59 +0000

    chore(template): merge template changes :up:
    Signed-off-by: PrinsFrank <github-action@actions-template-sync.noreply.github.com>

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -8,12 +8,22 @@ return (new PhpCsFixer\Config())->setRules(
         'strict_param'                 => true,
         'blank_line_after_opening_tag' => false,
         'no_extra_blank_lines'         => true,
-        'class_attributes_separation'  => true,
+        'class_attributes_separation'  => ['elements' => ['const' => 'only_if_meta', 'method' => 'one', 'property' => 'only_if_meta', 'trait_import' => 'only_if_meta', 'case' => 'only_if_meta']],
         'array_syntax'                 => ['syntax' => 'short'],
-        'binary_operator_spaces'       => ['default' => 'align_single_space']
+        'binary_operator_spaces'       => ['default' => 'single_space'],
+        'no_unused_imports'            => true,
+        'array_indentation'            => true,
+        'no_alias_functions'           => true,
+        'phpdoc_line_span'             => ['property' => 'single', 'method' => 'single', 'const' => 'single'],
+        'phpdoc_order'                 => ['order' => ['deprecated', 'internal', 'standard', 'source', 'template', 'implements', 'extends', 'covers', 'coversDefaultClass', 'param', 'throws', 'return']],
+        'phpdoc_param_order'           => true,
+        'phpdoc_separation'            => ['groups' => [['deprecated', 'internal'], ['standard', 'source'], ['covers', 'coversDefaultClass'], ['template', 'implements', 'extends', 'param', 'throws', 'return']]],
+        'phpdoc_trim'                  => true,
+        'concat_space'                 => ['spacing' => 'one'],
+        'braces_position'              => ['classes_opening_brace' => 'same_line', 'control_structures_opening_brace' => 'same_line', 'functions_opening_brace' => 'same_line'],
     ]
 )->setFinder(
     PhpCsFixer\Finder::create()
         ->exclude('vendor')
         ->in(__DIR__)
-);
+)->setCacheFile('./cache/.php-cs-fixer.cache');
